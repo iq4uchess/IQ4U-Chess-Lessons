84251cc697a6c7b1b7baf96c76cf047b1fc3490b
iq4uchess <iq4uchess@gmail.com>
2025-12-30 20:44:45 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./styles/chessground.base.css";
 import "./styles/chessground.brown.css";
 import "./styles/chessground.cburnett.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
