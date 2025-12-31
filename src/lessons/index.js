6ac834f8192b4102eb827da84655f523eae4134f
iq4uchess <iq4uchess@gmail.com>
2025-12-31 11:10:54 +0530

    Create index.js

---
+++ src/lessons/index.js
@@ -0,0 +1,134 @@
+/* AUTO-GENERATED — DO NOT EDIT MANUALLY */
+
+/* ======================================================
+   BEGINNER LEVEL 1
+====================================================== */
+export { default as lesson1_thechessboard } from "./lesson1_thechessboard";
+export { default as lesson2_thepawn } from "./lesson2_thepawn";
+export { default as lesson3_therook } from "./lesson3_therook";
+export { default as lesson4_thebishop } from "./lesson4_thebishop";
+export { default as lesson5_thequeen } from "./lesson5_thequeen";
+export { default as lesson6_theknight } from "./lesson6_theknight";
+export { default as lesson7_theking } from "./lesson7_theking";
+export { default as lesson8_theopening } from "./lesson8_theopening";
+export { default as lesson9_middlegame } from "./lesson9_middlegame";
+export { default as lesson10_matein1 } from "./lesson10_matein1";
+export { default as lesson11_matein2 } from "./lesson11_matein2";
+export { default as lesson12_italianopening } from "./lesson12_italianopening";
+
+/* ======================================================
+   BEGINNER LEVEL 2
+====================================================== */
+export { default as lesson13_notation } from "./lesson13_notation";
+export { default as lesson14_checkmate } from "./lesson14_checkmate";
+export { default as lesson15_pin } from "./lesson15_pin";
+export { default as lesson16_skewer } from "./lesson16_skewer";
+export { default as lesson17_discoveredattack } from "./lesson17_discoveredattack";
+export { default as lesson18_doublecheck } from "./lesson18_doublecheck";
+export { default as lesson19_fork } from "./lesson19_fork";
+export { default as lesson20_backrankmate } from "./lesson20_backrankmate";
+export { default as lesson21_smotheredmate } from "./lesson21_smotheredmate";
+export { default as lesson22_development } from "./lesson22_development";
+export { default as lesson23_kingsafety } from "./lesson23_kingsafety";
+export { default as lesson24_centre } from "./lesson24_centre";
+export { default as lesson25_exchange } from "./lesson25_exchange";
+export { default as lesson26_activity } from "./lesson26_activity";
+export { default as lesson27_material } from "./lesson27_material";
+export { default as lesson28_pawnstructure } from "./lesson28_pawnstructure";
+export { default as lesson29_positions } from "./lesson29_positions";
+export { default as lesson30_tworookendgame } from "./lesson30_tworookendgame";
+export { default as lesson31_queenendgame } from "./lesson31_queenendgame";
+export { default as lesson32_singlerook } from "./lesson32_singlerook";
+export { default as lesson33_twobishopmate } from "./lesson33_twobishopmate";
+export { default as lesson34_mateconfig } from "./lesson34_mateconfig";
+export { default as lesson35_combination } from "./lesson35_combination";
+export { default as lesson36_typicalcombo } from "./lesson36_typicalcombo";
+export { default as lesson37_openingintro } from "./lesson37_openingintro";
+export { default as lesson38_middlegameintro } from "./lesson38_middlegameintro";
+export { default as lesson39_endgameintro } from "./lesson39_endgameintro";
+export { default as lesson40_gambits } from "./lesson40_gambits";
+export { default as lesson41_rookbasic } from "./lesson41_rookbasic";
+export { default as lesson42_bishopbasic } from "./lesson42_bishopbasic";
+export { default as lesson43_knightbasic } from "./lesson43_knightbasic";
+export { default as lesson44_queenbasic } from "./lesson44_queenbasic";
+export { default as lesson45_beginner_test } from "./lesson45_beginner_test";
+
+/* ======================================================
+   INTERMEDIATE LEVEL 1
+====================================================== */
+export { default as lesson50_backrankweak } from "./lesson50_backrankweak";
+export { default as lesson51_attack1 } from "./lesson51_attack1";
+export { default as lesson52_attack2 } from "./lesson52_attack2";
+export { default as lesson53_kingcentre } from "./lesson53_kingcentre";
+export { default as lesson54_oppositesides } from "./lesson54_oppositesides";
+export { default as lesson55_devadv } from "./lesson55_devadv";
+export { default as lesson56_elimination } from "./lesson56_elimination";
+export { default as lesson57_space } from "./lesson57_space";
+export { default as lesson58_piecepos } from "./lesson58_piecepos";
+export { default as lesson59_weaksq } from "./lesson59_weaksq";
+export { default as lesson60_pawnweak } from "./lesson60_pawnweak";
+export { default as lesson61_bishopmid } from "./lesson61_bishopmid";
+export { default as lesson62_knightmid } from "./lesson62_knightmid";
+export { default as lesson63_passedpawnmid } from "./lesson63_passedpawnmid";
+export { default as lesson64_rookmid } from "./lesson64_rookmid";
+export { default as lesson65_queenmid } from "./lesson65_queenmid";
+export { default as lesson66_exchanging } from "./lesson66_exchanging";
+export { default as lesson67_closedcentres } from "./lesson67_closedcentres";
+export { default as lesson68_opencentres } from "./lesson68_opencentres";
+export { default as lesson69_fixedcentres } from "./lesson69_fixedcentres";
+export { default as lesson70_mobilecentres } from "./lesson70_mobilecentres";
+export { default as lesson71_evaluation } from "./lesson71_evaluation";
+export { default as lesson72_planning } from "./lesson72_planning";
+export { default as lesson73_unexpected } from "./lesson73_unexpected";
+export { default as lesson74_perpetual } from "./lesson74_perpetual";
+export { default as lesson75_overloaded } from "./lesson75_overloaded";
+export { default as lesson76_clearance } from "./lesson76_clearance";
+export { default as lesson77_openingprep } from "./lesson77_openingprep";
+export { default as lesson78_passedpawnend } from "./lesson78_passedpawnend";
+export { default as lesson79_advpawn1 } from "./lesson79_advpawn1";
+export { default as lesson80_advpawn2 } from "./lesson80_advpawn2";
+export { default as lesson81_advrook } from "./lesson81_advrook";
+export { default as lesson82_advqueen } from "./lesson82_advqueen";
+export { default as lesson83_bishopknight } from "./lesson83_bishopknight";
+export { default as lesson84_endtactics } from "./lesson84_endtactics";
+export { default as lesson85_inter_test } from "./lesson85_inter_test";
+
+/* ======================================================
+   ADVANCED
+====================================================== */
+export { default as lesson100_zugzwang } from "./lesson100_zugzwang";
+export { default as lesson101_blocking } from "./lesson101_blocking";
+export { default as lesson102_usingpins } from "./lesson102_usingpins";
+export { default as lesson103_deflections } from "./lesson103_deflections";
+export { default as lesson104_decoys } from "./lesson104_decoys";
+export { default as lesson105_discoveredattacks } from "./lesson105_discoveredattacks";
+export { default as lesson106_discoveredcheck } from "./lesson106_discoveredcheck";
+export { default as lesson107_xrayattack } from "./lesson107_xrayattack";
+export { default as lesson108_incorrectcombinations } from "./lesson108_incorrectcombinations";
+export { default as lesson109_knightvsbishopmid } from "./lesson109_knightvsbishopmid";
+export { default as lesson110_winningactivity } from "./lesson110_winningactivity";
+export { default as lesson111_oppositecolorbishops } from "./lesson111_oppositecolorbishops";
+export { default as lesson112_intuitivesacrifices } from "./lesson112_intuitivesacrifices";
+export { default as lesson113_advancedplanning } from "./lesson113_advancedplanning";
+export { default as lesson114_hangingpawns } from "./lesson114_hangingpawns";
+export { default as lesson115_activedefence } from "./lesson115_activedefence";
+export { default as lesson116_variationcalc1 } from "./lesson116_variationcalc1";
+export { default as lesson117_variationcalc2 } from "./lesson117_variationcalc2";
+export { default as lesson118_takingrisk } from "./lesson118_takingrisk";
+export { default as lesson119_initiative } from "./lesson119_initiative";
+export { default as lesson120_destructivesacrifices } from "./lesson120_destructivesacrifices";
+export { default as lesson121_breakingcentre } from "./lesson121_breakingcentre";
+export { default as lesson122_minorityattack } from "./lesson122_minorityattack";
+export { default as lesson123_exchangesacrifice } from "./lesson123_exchangesacrifice";
+export { default as lesson124_pawnbreak } from "./lesson124_pawnbreak";
+export { default as lesson125_reservetempo } from "./lesson125_reservetempo";
+export { default as lesson126_pawndefence } from "./lesson126_pawndefence";
+export { default as lesson127_knightvsbishopend } from "./lesson127_knightvsbishopend";
+export { default as lesson128_rookvspawn1 } from "./lesson128_rookvspawn1";
+export { default as lesson129_rookvspawn2 } from "./lesson129_rookvspawn2";
+export { default as lesson130_frontalassault } from "./lesson130_frontalassault";
+export { default as lesson131_simplifying } from "./lesson131_simplifying";
+export { default as lesson132_nevergiveup } from "./lesson132_nevergiveup";
+export { default as lesson133_selectedcombinations } from "./lesson133_selectedcombinations";
+export { default as lesson134_chessstudies } from "./lesson134_chessstudies";
+export { default as lesson135_advancedtest } from "./lesson135_advancedtest";
